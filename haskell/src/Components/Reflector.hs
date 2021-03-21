7ca269b337c4bd25b1b49ad68939b6f1381c2aa4
VKFisher <vladkf@gmail.com>
2021-03-21 21:41:05 +0700

    WIP

--- haskell/src/Components/Reflector.hs
+++ haskell/src/Components/Reflector.hs
@@ -1,2 +1,10 @@
 module Components.Reflector where
 
+import Enigma (EnigmaMonad)
+import Exceptions (EnigmaException (..))
+
+import ValidCharacters
+
+type Reflector = [ValidChar]
+
+reflector :: [Char] -> Reflector
